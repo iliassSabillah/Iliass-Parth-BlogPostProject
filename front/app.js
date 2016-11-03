01cac899628a1e911434255b61c82e6df5eb4329
iliass <iliass24@gmail.com>
2016-11-03 12:13:01 -0400

    input added in app.js

--- front/app.js
+++ front/app.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery'
 
-const Greeting = React.createClass({
-	render(){
-		return (
-			<div>Hello world</div>
-			)
-	}
+
+
+var NewPostForm = React.createClass({
+  getInitialState: function() {
+    return({input: ""})
+  },
+  handleChange: function(e) {
+    this.setState({input: e.target.value})
+  },
+  makeNewPost: function(e) {
+    e.preventDefault()
+    let body = this.state.input
+    $.ajax({
+      url: '/posts',
+      type: 'POST',
+      data: {title: body}
+    })
+  },
+  render: function() {
+    return(
+      <form
+        onSubmit={this.makeNewPost}>
+        <input
+          type="text"
+          placeholder="body"
+          onChange={this.handleChange}
+          value={this.state.input}></input>
+        <input type="submit"></input>
+      </form>
+    )
+  }
 })
+
 ReactDOM.render(
-  <Greeting/>,
+  <NewPostForm/>,
   document.getElementById('root')
 )
